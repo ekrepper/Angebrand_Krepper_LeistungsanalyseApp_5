80f56105e4cd499330abab8b2a40a88dfc93a04f
ekrepper <e.krepper@mci4me.at>
2024-04-11 11:02:10 +0200

    final commit

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from my_functions import build_experiment, estimate_max_hr, build_person
 
 # gewünschten Input vom User abfragen
 def get_user_input():
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    sex = input("Enter sex (male/female): ")
-    age = int(input("Enter age in years: "))
+    first_name_supervisor = input("Enter supervisor's first name: ")
+    last_name_supervisor = input("Enter supervisors's last name: ")
+    sex_supervisor = input("Enter supervisor's sex (male/female): ")
+    age_supervisor = int(input("Enter supervisor's age in years: "))
+    first_name_user = input("Enter users's first name: ")
+    last_name_user = input("Enter user's last name: ")
+    sex_user = input("Enter user's sex (male/female): ")
+    age_user = int(input("Enter user's age in years: "))
     experiment_name = input("Enter experiment name: ")
-    date = input("Enter date (e.g., YYYY-MM-DD): ")
-    return first_name, last_name, sex, age, experiment_name, date
+    experiment_date = input("Enter experiment date (e.g., YYYY-MM-DD): ")
+    return first_name_supervisor, last_name_supervisor, sex_supervisor, age_supervisor, first_name_user, last_name_user, sex_user, age_user, experiment_name, experiment_date
+
 
 # Hauptfunktion, welche alle anderen Funktionen aufruft
 def main():
-    first_name, last_name, sex, age, experiment_name, date = get_user_input()
-    max_hr = estimate_max_hr(age, sex)
-    supervisor = build_person(first_name, last_name, sex, age)  
-    subject = build_person(first_name, last_name, sex, age)
-    experiment = build_experiment(experiment_name, date, supervisor, subject)
+    first_name_supervisor, last_name_supervisor,sex_supervisor, age_supervisor, first_name_user, last_name_user, sex_user , age_user, experiment_name, experiment_date = get_user_input()
+    max_hr = estimate_max_hr(age_user, sex_user)
+    supervisor = build_person(first_name_supervisor, last_name_supervisor, sex_supervisor, age_supervisor)
+    subject = build_person(first_name_user, last_name_user, sex_user, age_user)
+    experiment = build_experiment(experiment_name, experiment_date, supervisor, subject)
     save_experiment_to_file(experiment)
 
 # Funktion, um das Experiment in eine JSON-Datei zu speichern
