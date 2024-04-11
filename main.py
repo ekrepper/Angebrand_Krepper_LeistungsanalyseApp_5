0872bb69223c2cbaa764ee8a437f93f700a87458
ekrepper <e.krepper@mci4me.at>
2024-04-11 09:20:55 +0200

    erste schritte

---
+++ main.py
@@ -0,0 +1,5 @@
+#welches die Funktionen nutzt um Dictionary von Experiment zu erstellen & zu printen, indem es den user nach benötigten Daten frägt
+
+print('Enter your name:')
+x = input()
+print('Hello, ' + x)
