7a30f3fdf430aab96dde993bdbc67501805e121d
ekrepper <e.krepper@mci4me.at>
2024-04-16 07:44:12 +0200

    Classes erstellt

---
+++ my_classes.py
@@ -0,0 +1,34 @@
+import json
+
+class Person():
+    def __init__(self, first_name, last_name, age, sex):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.sex = sex
+
+    def estimate_max_hr(self):
+        if sex == "male":
+            max_hr_bpm =  223 - 0.9 * age_years
+        elif sex == "female":
+            max_hr_bpm = 226 - 1.0 *  age_years
+        else:
+            max_hr_bpm  = input("Enter maximum heart rate:")
+        return int(max_hr_bpm)
+        
+
+    def save(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.__dict__, file)
+
+class Experiment():
+    def __init__(self, experiment_name, date, supervisor, subject):
+        self.experiment_name = experiment_name
+        self.date = date
+        self.supervisor = supervisor
+        self.subject = subject
+
+    def save(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.__dict__, file)
+
